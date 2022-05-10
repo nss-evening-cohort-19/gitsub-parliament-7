4c4d61a92b521d5a0f0e9735ff8682d97840bd1c
Rochelle Mikel Rossman <102299152+rochelle-rossman@users.noreply.github.com>
2022-05-09 20:20:08 -0500

    began packages data

---
+++ data.js
@@ -0,0 +1,19 @@
+const packages = [
+  {
+    id: 1,
+    name: "Docker",
+    description: "A software platform used for building applications based on containers — small and lightweight execution environments.",
+    image: "https://www.howtogeek.com/wp-content/uploads/csit/2021/09/993634a1.png?width=1198&trim=1,1&bg-color=000&pad=1,1",
+    link: "https://docs.github.com/en/packages/working-with-a-github-packages-registry/working-with-the-docker-registry"
+  },
+  {
+    id: 2,
+    name: "Apache Maven",
+    description: "A default package manager used for the Java programming language and the Java runtime environment.",
+    image: "https://upload.wikimedia.org/wikipedia/commons/thumb/7/7e/Apache_Feather_Logo.svg/1158px-Apache_Feather_Logo.svg.png",
+  },
+  {
+    
+  }
+
+]
