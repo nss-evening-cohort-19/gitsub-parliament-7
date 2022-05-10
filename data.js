5109e0fc5e996f9a8114db2b922aaa128360eeb0
Christian Black <blackl3@gmail.com>
2022-05-09 20:08:56 -0500

    Added project data set

---
+++ data.js
@@ -0,0 +1,45 @@
+const projectDataSet = [
+  {
+    id: 1, 
+    name: "Gitsub", 
+    description: "Worldlets birth corpus callosum are creatures of the cosmos Rig Veda science? Made in the interiors of collapsing stars descended from astronomers how far away inconspicuous motes of rock and gas colonies colonies. Another world finite but unbounded made in the interiors of collapsing stars muse about from which we spring a mote of dust suspended in a sunbeam? The sky calls to us vanquish the impossible with pretty stories for which there's little good evidence made in the interiors of collapsing stars Sea of Tranquility hearts of the stars?",
+    updatedTimeStamp: "05-10-2022", 
+    isPublic: true, 
+    link: "https://google.com"
+  },
+  {
+    id: 2, 
+    name: "Sorting Hat Project Board", 
+    description: "Worldlets birth corpus callosum are creatures of the cosmos Rig Veda science? Made in the interiors of collapsing stars descended from astronomers how far away inconspicuous motes of rock and gas colonies colonies. Another world finite but unbounded made in the interiors of collapsing stars muse about from which we spring a mote of dust suspended in a sunbeam? The sky calls to us vanquish the impossible with pretty stories for which there's little good evidence made in the interiors of collapsing stars Sea of Tranquility hearts of the stars?",
+    updatedTimeStamp: "05-10-2022", 
+    isPublic: true, 
+    link: "https://google.com"
+  },
+  {
+    id: 3, 
+    name: "Loops data project", 
+    description: "Globular star cluster cosmos dispassionate extraterrestrial observer light years of brilliant syntheses dream of the mind's eye? A mote of dust suspended in a sunbeam hearts of the stars venture muse about billions upon billions two ghostly white figures in coveralls and helmets are softly dancing. The ash of stellar alchemy vastness is bearable only through love gathered by gravity concept of the number one star stuff harvesting star light kindling the energy hidden in matter.",
+    updatedTimeStamp: "05-09-2022", 
+    isPublic: true, 
+    link: "https://google.com"
+  },
+  {
+    id: 4, 
+    name: "Assignment-product-cards", 
+    description: "Hypatia a billion trillion colonies venture white dwarf something incredible is waiting to be known. Finite but unbounded worldlets muse about cosmic ocean worldlets dispassionate extraterrestrial observer. Stirred by starlight star stuff harvesting star light vanquish the impossible a still more glorious dawn awaits the only home we've ever known vanquish the impossible. Extraordinary claims require extraordinary evidence extraordinary claims require extraordinary evidence vastness is bearable only through love extraordinary claims require extraordinary evidence the only home we've ever known extraordinary claims require extraordinary evidence.",
+    updatedTimeStamp: "05-08-2022", 
+    isPublic: true, 
+    link: "https://google.com"
+  },
+  {
+    id: 5, 
+    name: "LAB-word-counter", 
+    description: "Hypatia a billion trillion colonies venture white dwarf something incredible is waiting to be known. Finite but unbounded worldlets muse about cosmic ocean worldlets dispassionate extraterrestrial observer. Stirred by starlight star stuff harvesting star light vanquish the impossible a still more glorious dawn awaits the only home we've ever known vanquish the impossible. Extraordinary claims require extraordinary evidence extraordinary claims require extraordinary evidence vastness is bearable only through love extraordinary claims require extraordinary evidence the only home we've ever known extraordinary claims require extraordinary evidence.",
+    updatedTimeStamp: "05-08-2022", 
+    isPublic: true, 
+    link: "https://google.com"
+  },
+]
+
+
+export { projectDataSet }
