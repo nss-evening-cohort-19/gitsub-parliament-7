da05868237edadbabcc6c0f2b69646bf6e76c435
Brett Hughes <twofivinclimber@gmail.com>
2022-05-09 20:19:33 -0500

    added nav bar

--- main.js
+++ main.js
@@ -1,1 +1,28 @@
 console.log("connected");
+
+const renderToDom = (divId, textToRender) => {
+  selectedDiv = document.querySelector(divId);
+  selectedDiv.innerHTML = textToRender;
+};
+
+const renderNav = () => {
+  let domString = `<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#"></a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav">
+        <a class="nav-link active" aria-current="page" href="index.html">📖 Overview</a>
+        <a class="nav-link active" href="repo.html">🖥 Repositories</a>
+        <a class="nav-link active" href="project.html">🧮 Projects</a>
+        <a class="nav-link active" href="package.html">📦 Packages</a>
+      </div>
+    </div>
+  </div>
+</nav>`;
+  renderToDom("#nav-bar", domString);
+};
+
+renderNav();
