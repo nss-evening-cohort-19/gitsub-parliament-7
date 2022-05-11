c42aa8e7b21fb4ac8d5f29188584a12c2d478ed1
Brett Hughes <twofivinclimber@gmail.com>
2022-05-10 20:01:17 -0500

    added about me section

--- main.js
+++ main.js
@@ -27,9 +27,9 @@ const renderNav = () => {
 
 const renderProfile = () => {
   let domString = `<div class="card" style="width: 18rem;">
-<img src="..." class="card-img-top" alt="...">
+<img src="snoop.png" class="card-img-top" alt="...">
 <div class="card-body">
-  <h3 class="card-title">Steve Jobs</h3>
+  <h3 class="card-title">Snoop Dogg</h3>
   <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
   <button type="button" class="btn btn-secondary btn-sm">Follow</button>
   <button type="button" class="btn btn-secondary btn-sm">💓 Sponsor</button>
@@ -43,8 +43,8 @@ const renderProfile = () => {
 <div class="card-body">
 <ul class="list-group list-group-flush">
   <li class="list-group-item">📍 New York, New York</li>
-  <a href="#"><li href="#" class="list-group-item">📩 github@aboutmonica.com</li></a>
-  <a href="#"><li class="list-group-item">⛓ https://www.aboutmonica.com</li></a>
+  <a href="#"><li href="#" class="list-group-item">📩 github@aboutsnoop.com</li></a>
+  <a href="#"><li class="list-group-item">⛓ https://www.aboutsnoop.com</li></a>
   <a href="#"><li  class="list-group-item">🕊 @indigitacolor</li></a>
 </ul>
 </div>
@@ -76,6 +76,34 @@ const renderProfile = () => {
 </div>
 </div>`;
   renderToDom("#profile", domString);
+};
+
+const renderAbout = () => {
+  const domString = `<div class="card">
+  <div class="card-body">
+    <h5 class="card-title">Hi I'm Snoop Dogg 👋🏾 🎤</h5>
+    <img src="..." class="card-img-bottom" alt="...">
+    <p class="card-text">"I am thrilled and appreciative of the opportunity to acquire the iconic and culturally significant Death Row Records brand, which has immense untapped future value," the 50-year-old Snoop Dogg said in a statement. "It feels good to have ownership of the label I was part of at the beginning of my career and as one of the founding members. This is an extremely meaningful moment for me."</p>
+    <div class="card mb-3" style="max-width: 540px;">
+  <div class="row g-0">
+    <div class="col-md-4">
+      <img src="..." class="img-fluid rounded-start" alt="...">
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h5 class="card-title">Find Me Around The Wen</h5>
+        <ul>
+          <li> Learning to Rap in the <a href="#">Community</a></li>
+          <li> Turning the knobs in the <a href="#">Studio</a></li>
+          <li> Check me out on the <a href="#">Socials</a></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</div>
+  </div>
+</div>`;
+  renderToDom("#aboutMe", domString);
 };
 
 const renderFooter = () => {
@@ -113,19 +141,17 @@ const renderProjectCards = (arr) => {
     <div class="card-header">
     Projects
     </div>
-    <ul class="list-group list-group-flush">`
-  for(const item of arr) {
-    domString += `<li class="list-group-item"><h5>${item.name}</h5> <p>${item.description}</p></li>`
+    <ul class="list-group list-group-flush">`;
+  for (const item of arr) {
+    domString += `<li class="list-group-item"><h5>${item.name}</h5> <p>${item.description}</p></li>`;
   }
-  domString += 
-  `</ul>
-    </div>`; 
-  renderToDom("#project-card-div", domString)
-}
-
-
+  domString += `</ul>
+    </div>`;
+  renderToDom("#project-card-div", domString);
+};
 
 renderNav();
 renderProfile();
+renderAbout();
 renderFooter();
 renderProjectCards(projectDataSet);
