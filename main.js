7583550d62864b5c46972d15ac086362c0804d89
Rochelle Mikel Rossman <102299152+rochelle-rossman@users.noreply.github.com>
2022-05-10 19:39:48 -0500

    packages cards and form rendering on the DOM

--- main.js
+++ main.js
@@ -54,5 +54,50 @@ const renderFooter = () => {
   renderToDom("#footer", domString);
 };
 
+const renderPackagesCards = (taco) => {
+  let domString = "";
+  for (const item of taco) {
+    domString += 
+`<div class="card" style="width: 18rem;">
+  <img src="${item.image}" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">${item.name}</h5>
+    <p class="card-text">${item.description}</p>
+    <a href="#" class="btn btn-primary">Learn More</a>
+  </div>
+</div>`;
+  }
+  renderToDom("#packagesCards", domString);
+};
+
+const renderPackagesForm = () => {
+  let domString = "";
+  domString = `<form>
+          <div class="form-floating mb-3">
+            <input class="form-control form-control-lg" type="text" placeholder="packageName" id="packageName" aria-label="packageName" required>
+            <label for="packageName">Package Name</label>
+          </div>
+          
+          <div class="form-floating mb-3">
+            <input class="form-control form-control-lg" type="text" placeholder="packageDescription" id="packageDescription" aria-label="packageDescription" required>
+            <label for="packageDescription">Package Decription</label>
+          </div>
+          
+          <div class="form-floating mb-3">
+            <input class="form-control form-control-lg" type="text" placeholder="imageURL" id="imageUrl" aria-label="imageUrl" required>
+            <label for="packageId">ImageURL</label>
+          </div>
+
+          <div class="form-floating mb-3">
+            <input class="form-control form-control-lg" type="text" placeholder="packageURL" id="packageURL" aria-label="packageURL" required>
+            <label for="packageId">Package URL</label>
+          </div>
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>`;
+  renderToDom("#packagesForm", domString);
+};
+
 renderNav();
 renderFooter();
+renderPackagesCards(packages);
+renderPackagesForm();
