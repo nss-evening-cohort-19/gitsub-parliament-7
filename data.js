41d7a2b202d42e9712a5d606340b46254af970de
Imad Ottallah <imadotta@gmail.com>
2022-05-10 19:42:56 -0500

    corrected broken nerge

--- data.js
+++ data.js
@@ -1,9 +1,63 @@
+const packages = [
+  {
+    id: 1,
+    name: "Docker",
+    description:
+      "A software platform used for building applications based on containers — small and lightweight execution environments.",
+    image:
+      "https://www.howtogeek.com/wp-content/uploads/csit/2021/09/993634a1.png?width=1198&trim=1,1&bg-color=000&pad=1,1",
+    link: "https://docs.github.com/en/packages/working-with-a-github-packages-registry/working-with-the-docker-registry",
+  },
+  {
+    id: 2,
+    name: "Apache Maven",
+    description:
+      "A default package manager used for the Java programming language and the Java runtime environment.",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/7/7e/Apache_Feather_Logo.svg/1158px-Apache_Feather_Logo.svg.png",
+    link: "https://docs.github.com/en/packages/working-with-a-github-packages-registry/working-with-the-apache-maven-registry",
+  },
+  {
+    id: 3,
+    name: "NuGet",
+    description:
+      "A free and open source package manager used for the Microsoft development platforms including .NET.",
+    image: "https://www.nuget.org/Content/gallery/img/logo-og-600x600.png",
+    link: "https://docs.github.com/en/packages/working-with-a-github-packages-registry/working-with-the-nuget-registry",
+  },
+  {
+    id: 4,
+    name: "RubyGems",
+    description:
+      "A standard format for distributing Ruby programs and libraries used for the Ruby programming language.",
+    image: "https://miro.medium.com/max/600/1*_iNboVBMYZqRhao6OwNYFg.png",
+    link: "https://docs.github.com/en/packages/working-with-a-github-packages-registry/working-with-the-rubygems-registry",
+  },
+  {
+    id: 5,
+    name: "npm",
+    description:
+      "A package manager for JavaScript, included with Node.js. npm makes it easy for developers to share and reuse code.",
+    image: "https://miro.medium.com/max/347/1*uJ5NcEBI4ik0x7K74pH1Sg.png",
+    link: "https://docs.github.com/en/packages/working-with-a-github-packages-registry/working-with-the-npm-registry",
+  },
+  {
+    id: 6,
+    name: "Containers",
+    description:
+      "A single place for your team to manage Docker images and decide who can see and access your images.",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRxiqEhSYJeXPcFa4KFooGp6-GngMWZ-k4v2A&usqp=CAU",
+    link: "https://docs.github.com/en/packages/working-with-a-github-packages-registry/working-with-the-container-registry",
+  },
+];
 const repo = [
   {
     id: 1,
     name: "Sara",
     description: "Test test test test",
     url: `https://www.nationalgeographic.com/animals/mammals/facts/domestic-cat`,
+    tags: ["netify","jamstack","medical"],
     pinned: true,
     primaryLang: "Java",
     branches: 4,
@@ -14,6 +68,7 @@ const repo = [
     name: "Mark",
     description: "Test test test test",
     url: `https://www.nationalgeographic.com/animals/mammalshttps://static01.nyt.com/images/2021/09/14/science/07CAT-STRIPES/07CAT-STRIPES-jumbo.jpg?quality=75&auto=webp/facts/domestic-cat`,
+    tags: ["netify","jamstack","medical"],
     pinned: true,
     primaryLang: "Python",
     branches: 3,
@@ -24,6 +79,7 @@ const repo = [
     name: "Mary",
     description: "Test test test test",
     url: `https://cdn.theatlantic.com/thumbor/viW9N1IQLbCrJ0HMtPRvXPXShkU=/0x131:2555x1568/976x549/media/img/mt/2017/06/shutterstock_319985324/original.jpg`,
+    tags: ["netify","jamstack","medical"],
     pinned: true,
     primaryLang: "Javascript",
     branches: 3,
@@ -34,6 +90,7 @@ const repo = [
     name: "Susan",
     description: "Test test test test",
     url: `https://static.scientificamerican.com/sciam/cache/file/32665E6F-8D90-4567-9769D59E11DB7F26_source.jpg?w=590&h=800&7E4B4CAD-CAE1-4726-93D6A160C2B068B2`,
+    
     pinned: true,
     primaryLang: "C+",
     branches: 3,
@@ -52,46 +109,50 @@ const repo = [
 ];
 const projectDataSet = [
   {
-    id: 1, 
-    name: "Gitsub", 
-    description: "Worldlets birth corpus callosum are creatures of the cosmos Rig Veda science? Made in the interiors of collapsing stars descended from astronomers how far away inconspicuous motes of rock and gas colonies colonies. Another world finite but unbounded made in the interiors of collapsing stars muse about from which we spring a mote of dust suspended in a sunbeam? The sky calls to us vanquish the impossible with pretty stories for which there's little good evidence made in the interiors of collapsing stars Sea of Tranquility hearts of the stars?",
-    updatedTimeStamp: "05-10-2022", 
-    isPublic: true, 
-    link: "https://google.com"
+    id: 1,
+    name: "Gitsub",
+    description:
+      "Worldlets birth corpus callosum are creatures of the cosmos Rig Veda science? Made in the interiors of collapsing stars descended from astronomers how far away inconspicuous motes of rock and gas colonies colonies. Another world finite but unbounded made in the interiors of collapsing stars muse about from which we spring a mote of dust suspended in a sunbeam? The sky calls to us vanquish the impossible with pretty stories for which there's little good evidence made in the interiors of collapsing stars Sea of Tranquility hearts of the stars?",
+    updatedTimeStamp: "05-10-2022",
+    isPublic: true,
+    link: "https://google.com",
   },
   {
-    id: 2, 
-    name: "Sorting Hat Project Board", 
-    description: "Worldlets birth corpus callosum are creatures of the cosmos Rig Veda science? Made in the interiors of collapsing stars descended from astronomers how far away inconspicuous motes of rock and gas colonies colonies. Another world finite but unbounded made in the interiors of collapsing stars muse about from which we spring a mote of dust suspended in a sunbeam? The sky calls to us vanquish the impossible with pretty stories for which there's little good evidence made in the interiors of collapsing stars Sea of Tranquility hearts of the stars?",
-    updatedTimeStamp: "05-10-2022", 
-    isPublic: true, 
-    link: "https://google.com"
+    id: 2,
+    name: "Sorting Hat Project Board",
+    description:
+      "Worldlets birth corpus callosum are creatures of the cosmos Rig Veda science? Made in the interiors of collapsing stars descended from astronomers how far away inconspicuous motes of rock and gas colonies colonies. Another world finite but unbounded made in the interiors of collapsing stars muse about from which we spring a mote of dust suspended in a sunbeam? The sky calls to us vanquish the impossible with pretty stories for which there's little good evidence made in the interiors of collapsing stars Sea of Tranquility hearts of the stars?",
+    updatedTimeStamp: "05-10-2022",
+    isPublic: true,
+    link: "https://google.com",
   },
   {
-    id: 3, 
-    name: "Loops data project", 
-    description: "Globular star cluster cosmos dispassionate extraterrestrial observer light years of brilliant syntheses dream of the mind's eye? A mote of dust suspended in a sunbeam hearts of the stars venture muse about billions upon billions two ghostly white figures in coveralls and helmets are softly dancing. The ash of stellar alchemy vastness is bearable only through love gathered by gravity concept of the number one star stuff harvesting star light kindling the energy hidden in matter.",
-    updatedTimeStamp: "05-09-2022", 
-    isPublic: true, 
-    link: "https://google.com"
+    id: 3,
+    name: "Loops data project",
+    description:
+      "Globular star cluster cosmos dispassionate extraterrestrial observer light years of brilliant syntheses dream of the mind's eye? A mote of dust suspended in a sunbeam hearts of the stars venture muse about billions upon billions two ghostly white figures in coveralls and helmets are softly dancing. The ash of stellar alchemy vastness is bearable only through love gathered by gravity concept of the number one star stuff harvesting star light kindling the energy hidden in matter.",
+    updatedTimeStamp: "05-09-2022",
+    isPublic: true,
+    link: "https://google.com",
   },
   {
-    id: 4, 
-    name: "Assignment-product-cards", 
-    description: "Hypatia a billion trillion colonies venture white dwarf something incredible is waiting to be known. Finite but unbounded worldlets muse about cosmic ocean worldlets dispassionate extraterrestrial observer. Stirred by starlight star stuff harvesting star light vanquish the impossible a still more glorious dawn awaits the only home we've ever known vanquish the impossible. Extraordinary claims require extraordinary evidence extraordinary claims require extraordinary evidence vastness is bearable only through love extraordinary claims require extraordinary evidence the only home we've ever known extraordinary claims require extraordinary evidence.",
-    updatedTimeStamp: "05-08-2022", 
-    isPublic: true, 
-    link: "https://google.com"
+    id: 4,
+    name: "Assignment-product-cards",
+    description:
+      "Hypatia a billion trillion colonies venture white dwarf something incredible is waiting to be known. Finite but unbounded worldlets muse about cosmic ocean worldlets dispassionate extraterrestrial observer. Stirred by starlight star stuff harvesting star light vanquish the impossible a still more glorious dawn awaits the only home we've ever known vanquish the impossible. Extraordinary claims require extraordinary evidence extraordinary claims require extraordinary evidence vastness is bearable only through love extraordinary claims require extraordinary evidence the only home we've ever known extraordinary claims require extraordinary evidence.",
+    updatedTimeStamp: "05-08-2022",
+    isPublic: true,
+    link: "https://google.com",
   },
   {
-    id: 5, 
-    name: "LAB-word-counter", 
-    description: "Hypatia a billion trillion colonies venture white dwarf something incredible is waiting to be known. Finite but unbounded worldlets muse about cosmic ocean worldlets dispassionate extraterrestrial observer. Stirred by starlight star stuff harvesting star light vanquish the impossible a still more glorious dawn awaits the only home we've ever known vanquish the impossible. Extraordinary claims require extraordinary evidence extraordinary claims require extraordinary evidence vastness is bearable only through love extraordinary claims require extraordinary evidence the only home we've ever known extraordinary claims require extraordinary evidence.",
-    updatedTimeStamp: "05-08-2022", 
-    isPublic: true, 
-    link: "https://google.com"
+    id: 5,
+    name: "LAB-word-counter",
+    description:
+      "Hypatia a billion trillion colonies venture white dwarf something incredible is waiting to be known. Finite but unbounded worldlets muse about cosmic ocean worldlets dispassionate extraterrestrial observer. Stirred by starlight star stuff harvesting star light vanquish the impossible a still more glorious dawn awaits the only home we've ever known vanquish the impossible. Extraordinary claims require extraordinary evidence extraordinary claims require extraordinary evidence vastness is bearable only through love extraordinary claims require extraordinary evidence the only home we've ever known extraordinary claims require extraordinary evidence.",
+    updatedTimeStamp: "05-08-2022",
+    isPublic: true,
+    link: "https://google.com",
   },
-]
+];
 
-
-export { projectDataSet }
+export { projectDataSet, repo, packages };
--- main.js
+++ main.js
@@ -1,1 +1,77 @@
-console.log("connected");
+import { projectDataSet, repo, packages } from "./data.js";
+
+const renderToDom = (divId, textToRender) => {
+  const selectedDiv = document.querySelector(divId);
+  selectedDiv.innerHTML = textToRender;
+};
+
+const renderNav = () => {
+  let domString = `<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#"></a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav">
+        <a class="nav-link active" aria-current="page" href="index.html">📖 Overview</a>
+        <a class="nav-link active" href="repo.html">🖥 Repositories</a>
+        <a class="nav-link active" href="project.html">🧮 Projects</a>
+        <a class="nav-link active" href="package.html">📦 Packages</a>
+      </div>
+    </div>
+  </div>
+</nav>`;
+  renderToDom("#nav-bar", domString);
+};
+
+const renderFooter = () => {
+  let domString = `<footer><nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#"></a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav">
+        <a class="nav-link" href="#">2021 GitHub, Inc.</a>
+        <a class="nav-link active" href="#">Terms</a>
+        <a class="nav-link active" href="#">Privacy</a>
+        <a class="nav-link active" href="#">Security</a>
+        <a class="nav-link active" href="#">Status</a>
+        <a class="nav-link active" href="#">Help</a>
+        <a class="nav-link active" href="#">🙀</a>
+        <a class="nav-link active" href="#">Contact GitHub</a>
+        <a class="nav-link active" href="#">Pricing</a>
+        <a class="nav-link active" href="#">API</a>
+        <a class="nav-link active" href="#">Training</a>
+        <a class="nav-link active" href="#">Blog</a>
+        <a class="nav-link active" href="#">About</a>
+      </div>
+    </div>
+  </div>
+</nav></footer>`;
+  renderToDom("#footer", domString);
+};
+
+const reposOnDom = (array) => {
+let domString = "";
+for (const item of array){
+  domString = `
+  <div class="card" style="width: 18rem;">
+  <ul class="list-group list-group-flush">
+    <li class="list-group-item">${item.name}</li>
+    <li class="list-group-item">${item.description}</li>
+    <li class="list-group-item">A third item</li>
+  </ul>
+  <div class="card-footer">
+    Card footer
+  </div>
+</div>`
+};
+renderToDom("#repoCards", domString)
+};
+
+renderNav();
+renderFooter();
+reposOnDom(repo);
