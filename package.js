fff355287e5d88ac0a1850db8f57b5bf98065718
Rochelle Mikel Rossman <102299152+rochelle-rossman@users.noreply.github.com>
2022-05-12 11:47:46 -0500

    completed new package form funtionality

--- package.js
+++ package.js
@@ -1,15 +1,21 @@
 import {renderToDom} from "./utils/renderToDom.js"
 import { packages } from "./data.js"
 
-const renderPackagesCards = (taco) => {
+const packageID = () => {
+  packages.forEach((pack, index) => {
+    pack.id = index + 1;
+  });
+};
+
+const renderPackagesCards = (pack) => {
   let domString = "";
-  for (const item of taco) {
+  for (const item of pack) {
     domString += `<div class="card" style="width: 18rem;">
   <img src="${item.image}" class="card-img-top" alt="...">
   <div class="card-body">
     <h5 class="card-title">${item.name}</h5>
     <p class="card-text">${item.description}</p>
-    <a href="#" class="btn btn-primary">Learn More</a>
+    <a target="_blank" rel="noopener noreferrer"href=${item.link} class="btn btn-primary">Learn More</a>
   </div>
 </div>`;
   }
@@ -20,22 +26,22 @@ const renderPackagesForm = () => {
   let domString = "";
   domString = `<form>
           <div class="form-floating mb-3">
-            <input class="form-control form-control-lg" type="text" placeholder="packageName" id="packageName" aria-label="packageName" required>
+            <input id="packageName" class="form-control form-control-lg" type="text" placeholder="packageName" id="packageName" aria-label="packageName" required>
             <label for="packageName">Package Name</label>
           </div>
           
           <div class="form-floating mb-3">
-            <input class="form-control form-control-lg" type="text" placeholder="packageDescription" id="packageDescription" aria-label="packageDescription" required>
+            <input id="packageDescription" class="form-control form-control-lg" type="text" placeholder="packageDescription" id="packageDescription" aria-label="packageDescription" required>
             <label for="packageDescription">Package Decription</label>
           </div>
           
           <div class="form-floating mb-3">
-            <input class="form-control form-control-lg" type="text" placeholder="imageURL" id="imageUrl" aria-label="imageUrl" required>
+            <input id="imageUrl" class="form-control form-control-lg" type="text" placeholder="imageURL" id="imageUrl" aria-label="imageUrl" required>
             <label for="packageId">ImageURL</label>
           </div>
 
           <div class="form-floating mb-3">
-            <input class="form-control form-control-lg" type="text" placeholder="packageURL" id="packageURL" aria-label="packageURL" required>
+            <input id="packageUrl" class="form-control form-control-lg" type="text" placeholder="packageURL" id="packageURL" aria-label="packageURL" required>
             <label for="packageId">Package URL</label>
           </div>
   <button type="submit" class="btn btn-primary">Submit</button>
@@ -43,5 +49,26 @@ const renderPackagesForm = () => {
   renderToDom("#packagesForm", domString);
 };
 
+function packageEvent() {
+  const packageForm = document.querySelector('form');
+  packageForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    let newPackage = {
+      name: document.querySelector("#packageName").value,
+      description: document.querySelector("#packageDescription").value,
+      image: document.querySelector("#imageUrl").value,
+      link: document.querySelector("#packageUrl").value,
+    };
+    packages.push(newPackage);
+    packageID();
+    renderPackagesCards(packages);
+    packageForm.reset();
+  });
+
+
+};
+
 renderPackagesCards(packages);
 renderPackagesForm();
+packageID();
+packageEvent();
